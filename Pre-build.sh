e5c2e1df1fd8592cbf30eac782c2d739d37da2e1
Alejandro José Junior Cruz Mocarro <144296599+AleJCruz@users.noreply.github.com>
2024-02-05 11:10:28 -0500

    Update Pre-build.sh

--- Pre-build.sh
+++ Pre-build.sh
@@ -2,11 +2,6 @@
 echo "Part 3: Pre-Build"
 echo "------------------"
 
-# Configura el PATH para usar Gradle 7.2
-export PATH=/opt/gradle/gradle-7.2/bin:$PATH
-
-# Navega al directorio del proyecto
-cd /home/business.account/project/helloworld/
 
 # Clean and build the project
 gradle clean build
