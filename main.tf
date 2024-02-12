283a938f90f5fe96053e2e8b52e364a350f191dd
Alejandro José Junior Cruz Mocarro <144296599+AleJCruz@users.noreply.github.com>
2024-02-12 12:02:20 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,23 @@
 terraform {
+  required_version = ">= 0.13, < 0.14"  # Adjust the version range accordingly
+
   required_providers {
-    source = "hashicorp/azurerm"
-    version = "~> 2.65"
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.65"  # Adjust the version constraint accordingly
+    }
   }
 }
+
 provider "azurerm" {
   features {}
 }
+
 resource "azurerm_resource_group" "example" {
   name     = "c"
   location = "East US"
 }
+
 resource "azurerm_virtual_network" "example" {
   name                = "example-network"
   address_space       = ["10.0.0.0/16"]
