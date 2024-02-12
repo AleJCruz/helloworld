f559c86ff54c43314f0ade84302dc1b45f6d1024
Alejandro José Junior Cruz Mocarro <144296599+AleJCruz@users.noreply.github.com>
2024-02-12 11:54:57 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,14 @@
 provider "azurerm" {
-  version = "=2.53.0"  # You can adjust the version based on your needs
-  features = {}
   subscription_id = "8aa7433f-6015-4397-9889-1d2d794e633d"
   client_id       = "8aa7433f-6015-4397-9889-1d2d794e633d"
   client_secret   = "wOi8Q~aM-tsekMej543FtVELrOxP9H26pFjWOaCL"
   tenant_id       = "e3de6039-942b-4f3e-b8fa-8d8008f8fd74"
+  features        = {}  # Add this line
 }
 
 resource "azurerm_resource_group" "example" {
   name     = "c"
-  location = "eastus"
+  location = "East US"
 }
 
 resource "azurerm_virtual_network" "example" {
